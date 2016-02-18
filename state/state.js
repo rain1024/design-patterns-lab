3bee592fcf35699bd7b0acf9287f78a8174aa97d
Vu Anh <brother.rain.1024@gmail.com>
2016-02-18 10:45:10 +0700

    add state pattern

---
+++ state/state.js
@@ -0,0 +1,31 @@
+/**
+ * Created by rain on 2/18/2016.
+ */
+
+function On(){
+  this.click = function(context){
+    console.log("Turn On");
+    context.setState(new Off());
+  }
+}
+
+function Off(){
+  this.click = function(){
+    console.log("Turn Off");
+    context.setState(new On());
+  }
+
+}
+
+function Context(){
+  this.current = new Off();
+
+  this.setState = function(state){
+    this.current = state;
+  };
+
+  this.click = function(){
+    this.current.click(context);
+  }
+}
+
