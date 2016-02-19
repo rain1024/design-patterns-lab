e50cb07bead6ce034e38c67119c86241cd6da0db
rain1024 <brother.rain.1024@gmail.com>
2016-02-19 11:00:52 +0700

    add factor method

---
+++ factory-method/code/main.js
@@ -0,0 +1,7 @@
+var phone = Factory.create("phone", "iPhone 6", "$700");
+console.log(phone);
+phone.call();
+
+var car = Factory.create("car", "LaFerrari FXX K", "$1.2M");
+console.log(car);
+car.run();
